927e23df7af58c84b72819fe6ca0c885278b7230
Thomas Young <yk_ecust_2007@163.com>
2019-10-24 09:33:54 +0800

    [feature] decorator, key argument test

--- 07-closure-deco/clockdeco_demo.py
+++ 07-closure-deco/clockdeco_demo.py
@@ -1,18 +1,31 @@
 # clockdeco_demo.py
 
 import time
+
 from clockdeco import clock
+
 
 @clock
 def snooze(seconds):
     time.sleep(seconds)
 
+
 @clock
 def factorial(n):
-    return 1 if n < 2 else n*factorial(n-1)
+    """阶乘函数"""
+    return 1 if n < 2 else n * factorial(n - 1)
 
-if __name__=='__main__':
+
+@clock
+def foo(a, *, b=100, **kwargs):
+    return a + b + sum(kwargs.values())
+
+
+if __name__ == '__main__':
     print('*' * 40, 'Calling snooze(.123)')
     snooze(.123)
     print('*' * 40, 'Calling factorial(6)')
     print('6! =', factorial(6))
+    print(factorial.__name__)
+    print(factorial.__doc__)
+    foo(10, c=10, d=15, b=12)
