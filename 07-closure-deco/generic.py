f0646d60f57bb6c66403a988b091ad8ba0fa8337
Thomas Young <yk_ecust_2007@163.com>
2019-10-25 23:40:07 +0800

    [feature] 注册集合类型

--- 07-closure-deco/generic.py
+++ 07-closure-deco/generic.py
@@ -23,30 +23,41 @@ htmlize(): generic function example
 
 # BEGIN HTMLIZE
 
-from functools import singledispatch
-from collections import abc
-import numbers
 import html
+import numbers
+from collections import abc
+from functools import singledispatch
+
 
 @singledispatch  # <1>
 def htmlize(obj):
+    '''不满足任何注册类型的入参会进入这里, 兜底用')'''
     content = html.escape(repr(obj))
     return '<pre>{}</pre>'.format(content)
 
+
 @htmlize.register(str)  # <2>
-def _(text):            # <3>
+def _(text):  # <3>
     content = html.escape(text).replace('\n', '<br>\n')
     return '<p>{0}</p>'.format(content)
+
 
 @htmlize.register(numbers.Integral)  # <4>
 def _(n):
     return '<pre>{0} (0x{0:x})</pre>'.format(n)
 
+
 @htmlize.register(tuple)  # <5>
 @htmlize.register(abc.MutableSequence)
+# @htmlize.register(abc.MutableSet)  # 注册集合类型
 def _(seq):
     inner = '</li>\n<li>'.join(htmlize(item) for item in seq)
     return '<ul>\n<li>' + inner + '</li>\n</ul>'
 
+
 # END HTMLIZE
 
+if __name__ == '__main__':
+    print(htmlize('你妈妈'))
+    print(htmlize(object()))
+    print(htmlize({1, 2, 3}))
